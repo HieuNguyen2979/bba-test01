8bea9836a0a72dcc0f82d8e56636894a79fef42f
test01-HieuNguyen <demonhunter2979@gmail.com>
2026-03-11 00:59:16 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,9 @@
+let playername = 'Mario';
+let currentLives = 3;
+const level1 = 25;
+const level2= 30;
+const level3=45;
+let total, avg;
+total = level1+level2+level3;
+avg = total/3;
+console.log(total%3);
